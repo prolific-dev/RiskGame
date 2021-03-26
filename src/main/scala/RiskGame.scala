5818654516e93c9a5f703794360e4fde37f243b1
prolific-dev <de391ago@htwg-konstanz.de>
2021-03-26 12:14:59 +0100

    modified first commit and setup of the basic project

--- src/main/scala/RiskGame.scala
+++
@@ -1,6 +0,0 @@
-object RiskGame {
-  def main(args: Array[String]): Unit = {
-    println("Welcome to Risk Game!")
-  }
-
-}
---
+++ src/main/scala/de/htwg/se/riskgame/RiskGame.scala
@@ -0,0 +1,12 @@
+package de.htwg.se.riskgame
+
+object RiskGame {
+  def main(args: Array[String]): Unit = {
+    println(helloWorld())
+  }
+
+  def helloWorld(): String = {
+    "Welcome to Risk Game!"
+  }
+
+}
